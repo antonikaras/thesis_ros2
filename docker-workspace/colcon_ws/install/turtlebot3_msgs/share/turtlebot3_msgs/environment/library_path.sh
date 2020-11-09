ef55f84046fd3a9531d4bec4953dbfdf3deaaf15
antonikaras <antonikaras1995@hotmail.com>
2020-11-09 15:34:26 +0100

    Minor changes

--- docker-workspace/colcon_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/environment/library_path.sh
+++ docker-workspace/colcon_ws/install/turtlebot3_msgs/share/turtlebot3_msgs/environment/library_path.sh
@@ -1,1 +1,1 @@
-/opt/ros/foxy/lib/python3.8/site-packages/ament_package/template/environment_hook/library_path.sh
+/opt/ros/dashing/lib/python3.6/site-packages/ament_package/template/environment_hook/library_path.sh
