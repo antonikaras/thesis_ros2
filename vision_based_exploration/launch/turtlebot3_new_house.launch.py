24f19bccfaf48380f2ad3490701d5ce7c31655ee
antonikaras <antonikaras1995@hotmail.com>
2021-01-22 11:48:05 +0100

    Added custom messages

--- vision_based_exploration/launch/turtlebot3_new_house.launch.py
+++ vision_based_exploration/launch/turtlebot3_new_house.launch.py
@@ -11,11 +11,16 @@ TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    world_file_name = TURTLEBOT3_MODEL + 'new_house'
-    world = os.path.join(get_package_share_directory('vision_based_exploration'),
+    #world_file_name = TURTLEBOT3_MODEL + '_new_house.model'
+    #world = os.path.join(get_package_share_directory('vision_based_exploration'),
+    #                     'worlds', world_file_name)
+    world_file_name = 'turtlebot3_houses/' + TURTLEBOT3_MODEL + '_new_house.model'
+    world = os.path.join(get_package_share_directory('turtlebot3_gazebo'),
                          'worlds', world_file_name)
-    launch_file_dir = os.path.join(get_package_share_directory('vision_based_exploration'), 'launch')
+    
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    print(world)
 
     return LaunchDescription([
         IncludeLaunchDescription(
@@ -30,7 +35,7 @@ def generate_launch_description():
                 os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
             ),
         ),
-
+    
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([launch_file_dir, '/robot_state_publisher.launch.py']),
             launch_arguments={'use_sim_time': use_sim_time}.items(),
