f8656869775bea0e76a7d54db6b3167e3becf359
antonikaras <antonikaras1995@hotmail.com>
2021-05-19 11:29:28 +0200

    added 2 maps

--- interactive_map_tester/launch/test_launch.launch.py
+++ interactive_map_tester/launch/test_launch.launch.py
@@ -1,34 +1,104 @@
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription , DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
-    interactive_map_tester_dir = get_package_share_directory('interactive_map_tester')
-    
-    cartographer_dir = get_package_share_directory('autonomous_exploration')
-    interactiveMapTester_dir = get_package_share_directory('interactive_map_tester')
-    cartographer_configuration = TURTLEBOT3_MODEL + '.lua'
+    # Get the launch directory
+    costmap_filters_demo_dir = get_package_share_directory('interactive_map_tester')
 
-    print(interactive_map_tester_dir)
-    print(cartographer_configuration)
+    # Create our own temporary YAML files that include substitutions
+    lifecycle_nodes = ['filter_mask_server']
 
-    return LaunchDescription([
-    DeclareLaunchArgument('use_sim_time', default_value='true'),
+    # Parameters
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    autostart = LaunchConfiguration('autostart')
+    params_file = LaunchConfiguration('params_file')
+    mask_yaml_file = LaunchConfiguration('mask')
 
-     IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(cartographer_dir + '/turtlebot3_cartographer.launch.py'),
-        launch_arguments={'use_sim_time': 'true',
-                          'cartographer_config_dir' : interactiveMapTester_dir,
-                          'configuration_basename' : cartographer_configuration,
-                          'cartographer_mode' : 'localization'}.items()
-        )
-                    
-    ])
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the nav2 stack')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+            'params_file',
+            default_value=os.path.join(costmap_filters_demo_dir, 'params', 'keepout_params.yaml'),
+            description='Full path to the ROS2 parameters file to use')
+
+    declare_mask_yaml_file_cmd = DeclareLaunchArgument(
+            'mask',
+            default_value=os.path.join(costmap_filters_demo_dir, 'maps', 'keepout_mask.yaml'),
+            description='Full path to filter mask yaml file to load')
+
+    # Make re-written yaml
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'yaml_filename': mask_yaml_file}
+
+    configured_params = RewrittenYaml(
+        source_file=params_file,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+
+    # Nodes launching commands
+    start_lifecycle_manager_cmd = Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_costmap_filters',
+            namespace=namespace,
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': autostart},
+                        {'node_names': lifecycle_nodes}])
+
+    start_map_server_cmd = Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='filter_mask_server',
+            namespace=namespace,
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[configured_params])
+    '''
+    start_costmap_filter_info_server_cmd = Node(
+            package='nav2_map_server',
+            executable='costmap_filter_info_server',
+            name='costmap_filter_info_server',
+            namespace=namespace,
+            output='screen',
+            emulate_tty=True,  # https://github.com/ros2/launch/issues/188
+            parameters=[configured_params])
+    '''
+    ld = LaunchDescription()
+
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_mask_yaml_file_cmd)
+
+    ld.add_action(start_lifecycle_manager_cmd)
+    ld.add_action(start_map_server_cmd)
+    #ld.add_action(start_costmap_filter_info_server_cmd)
+
+    return ld
